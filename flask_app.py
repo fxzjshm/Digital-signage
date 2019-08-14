08b6c47827f89addecaf9f38581e8f1f1defe0b6
fxzjshm <364467005@qq.com>
2019-08-14 12:10:06 +0800

    Fix url encoding

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,22 @@
 # https://fxzjshm.pythonanywhere.com/Digital-signage
-from flask import Flask, request, Response
+
+from flask import Flask, Response
 from flask_script import Manager
 import urllib.request
+from urllib.parse import quote
+import string
 
 app = Flask(__name__)
 manager = Manager(app)
 
 @app.route('/<path:path>')
 def test(path):
-    response = urllib.request.urlopen('https://fxzjshm.github.io/' + path)
-#    print response.read()
+
+    # https://blog.csdn.net/qq_25406563/article/details/81253347
+    url = quote('https://fxzjshm.github.io/' + path, safe=string.printable)
+
+    response = urllib.request.urlopen(url)
+#   print response.read()
     res=Response(response.read())
     res.headers['Content-Type']=response.info()['Content-Type']
     return res, response.status
